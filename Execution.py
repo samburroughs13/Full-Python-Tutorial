8a0c3185a667ff8a66f0188fbdfba78b7078db9a
Sam Burroughs <samburroughs0709@gmail.com>
2024-04-01 23:16:39 -0400

    Add files via upload

---
+++ Execution.py
@@ -0,0 +1,4 @@
+print("Sam Burroughs")
+print('o----')
+print(' ||||')
+print('*' * 10)
---
+++ Variables.py
@@ -0,0 +1,14 @@
+price = 10
+rating = 4.9
+name = 'Sam'
+is_published = True
+print(price)
+
+# Exercise:
+# At a hospital, we check in a patient John Smith. He's 20 years old and is a new patient.
+# Define variables for name, age, and if he is a new or existing patient
+
+first_name = "John"
+last_name = "Smith"
+age = 20
+is_new_patient = True
